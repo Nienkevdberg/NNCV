11ca3e007db070d5db448e3d8cdda8aa9d2e23f4
Nienkevdberg <nienkevdberg11@gmail.com>
2026-03-26 12:59:52 +0100

    add deeplabv3

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,8 +3,8 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 16 \
-    --epochs 200 \
-    --lr 0.001 \
+    --epochs 100 \
+    --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "unet-training" \
