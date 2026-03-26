5d4d06c2a4b163dceffcaf88f7382eb2e0a02c5e
Nienkevdberg <nienkevdberg11@gmail.com>
2026-03-26 09:41:53 +0100

    fix resize

--- Final assignment/train.py
+++ Final assignment/train.py
@@ -147,7 +147,7 @@ def main(args):
     # Define the transforms to apply to the data
     img_transform = Compose([
         ToImage(),
-        Resize((512, 1024)),
+        Resize((256, 512)),
         RandomHorizontalFlip(p=0.5),
         ColorJitter(brightness=0.3, contrast=0.3, saturation=0.2, hue=0.02),
         GaussianBlur(kernel_size=5, sigma=(0.1, 2.0)),
@@ -158,21 +158,21 @@ def main(args):
     # Target transform (mask)
     target_transform = Compose([
         ToImage(),
-        Resize((512, 1024), interpolation=InterpolationMode.NEAREST),
+        Resize((256, 512), interpolation=InterpolationMode.NEAREST),
         RandomHorizontalFlip(p=0.5),
         ToDtype(torch.int64),  # no scaling
     ])
 
     valid_img_transform = Compose([
         ToImage(),
-        Resize((512, 1024)),
+        Resize((256, 512)),
         ToDtype(torch.float32, scale=True),
         Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
 
     valid_target_transform = Compose([
         ToImage(),
-        Resize((512, 1024), interpolation=InterpolationMode.NEAREST),
+        Resize((256, 512), interpolation=InterpolationMode.NEAREST),
         ToDtype(torch.int64),
     ])
 
