5889a0805eba24fc625a71ad8b588640f35bab3f
Nienkevdberg <nienkevdberg11@gmail.com>
2026-03-26 09:53:55 +0100

    fix epoch

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,7 +3,7 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 16 \
-    --epochs 10 \
+    --epochs 200 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
--- Final assignment/test_model.py
+++ Final assignment/test_model.py
@@ -8,7 +8,7 @@ model = Model(in_channels=3, n_classes=19).to(device)
 model.eval()
 
 # Fake input (zoals Cityscapes)
-x = torch.randn(1, 3, 512, 1024).to(device)
+x = torch.randn(1, 3, 256, 512).to(device)
 
 with torch.no_grad():
     y = model(x)
